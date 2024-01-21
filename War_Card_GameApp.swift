092ed5d2efa291611840edd139c8d3bde9d71dfb
Yazan Hawamdeh <96641766+yhawamdeh@users.noreply.github.com>
2024-01-20 19:12:57 -0600

    Add files via upload

---
+++ War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Yazan Hawamdeh on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
